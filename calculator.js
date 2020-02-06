018116eb3878dc330a31adddcac6154eb79835a5
aidan-kelly <it.is.aidan@gmail.com>
2020-02-06 14:34:55 -0700

    Removed global var. Now displays formatting errors as well.

--- calculator.js
+++ calculator.js
@@ -1,8 +1,9 @@
-let command_string = "";
-
 function addButtonListener(){
     //grabs all of our buttons
     let ourButtons = document.getElementsByTagName("button");
+
+    //we will use result to hold our command string
+    let command_string = document.getElementById("result").textContent;
 
     //loop through the buttons
     for(let i = 0; i < ourButtons.length; i++){
@@ -12,25 +13,37 @@ function addButtonListener(){
             let ourChar = ourButtons[i].textContent;
 
             if(ourChar === '='){
-                let result = eval(command_string);
-                document.getElementById("result").textContent = `${command_string} = ${result}`;
-                command_string = result;
 
+                //execute the command string
+                try{
+                    let result = eval(command_string);
+                    document.getElementById("result").textContent = `${command_string} = ${result}`;
+                    command_string = result;
+
+                //if the user entered in a poorly formatted equation
+                //catch the error and output it. 
+                }catch (error){
+                    document.getElementById("result").textContent = error.toString();
+                }
 
             }else if(ourChar === "←"){
+
+                //remove the last element from the command string
                 command_string = command_string.slice(0, -1);
                 document.getElementById("result").textContent = command_string;
 
             }else if(ourChar === "Clear"){
+
+                //clear the command string
                 command_string = "";
                 document.getElementById("result").textContent = command_string;
 
             }else{
+
+                //add the character to the command string
                 command_string += ourChar;
                 document.getElementById("result").textContent = command_string;
             }
-
-            console.log(command_string);
             
         });
     }
